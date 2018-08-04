bf969facc5216390245bfc42d03edf9bc42e3878
yiyizym <yiyizym@gmail.com>
2018-08-04 14:42:03 +0800

    use production mode vue

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/index.js',
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.js'
+            vue: 'vue/dist/vue.min.js'
         }
     },
     serve: {

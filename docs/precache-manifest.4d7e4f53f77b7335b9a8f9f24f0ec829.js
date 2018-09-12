4d296ed67c2de79d775004acde6520f4ed2d6315
zhuyimin <zhuyimin@meizu.com>
2018-09-12 18:02:27 +0800

    generate site

---
+++ docs/precache-manifest.4d7e4f53f77b7335b9a8f9f24f0ec829.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "95618ffe2bf4af1381de5a1184858a4d",
+    "url": "index.html"
+  },
+  {
+    "revision": "93c75315db699953aee0",
+    "url": "bundle.js"
+  }
+];
